2b6057eeac20f378711ef5e0945095a4f5c0e058
shabeeb <shabeeb.m@codelynks.com>
2025-04-30 10:53:00 +0530

    changed test file name

--- src/cred/tests/test_api.py
+++ src/cred/tests/test_api.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 @pytest.mark.django_db
-def test_authenticated_book_create(auth_client):
+def test_authenticated_cred_create(auth_client):
     data = {"title": "Test Book", "description": "Tester"}
     logger.info(f"{data=}")
     response = auth_client.post("/api/cred/", data, format="json")
